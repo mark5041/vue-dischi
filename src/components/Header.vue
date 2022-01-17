9b81e0b4ba4f325d9d4c3601ac76223dd08702e4
mark5041 <mark.beloso5041@gmail.com>
2022-01-17 16:38:12 +0100

    card

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    
+    <nav class="navbar">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+    </nav>
+
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/style.scss";
+    nav {
+        height: $header_height;
+        background-color: $bg_header;
+        img {
+            height: 2.5em;
+            margin-left: 0.7em;
+        }
+    }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,65 @@
+<template>
+    
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="card">
+                    <img src="https://picsum.photos/id/684/600/400" alt="">
+                    <h1>New Jersey</h1>
+                    <h3>New Jersey</h3>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/style.scss";
+    
+    main {
+        background-color: $bg_main;
+        height: $main_height;
+        width: 100%;
+
+        .container{
+            position: relative;
+            top: 7em;
+            .row {
+                column-gap: $card_gap;
+                row-gap: calc($card_gap / 2);
+                .card {
+                    flex-basis: $card_width;
+                    align-items: center;
+                    background-color: $bg_card;
+                    img {
+                        margin: 1em 0;
+                        width: 80%;
+                    }
+                    h1 {
+                        color: $title_color;
+                        font-size: $title_size;
+                    }
+                    h3 {
+                        color: $sub_title_color;
+                        font-size: $sub_title_size;
+                        margin-top: 1.5em;
+                        margin-bottom: 1em;
+                    }
+                }
+            }
+        }
+    }
+    
+</style>
